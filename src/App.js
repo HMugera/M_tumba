8196658463a9ba1c62fcb83936158643790549bd
Hmugera <mugerahughes@gmail.com>
2020-09-22 14:55:28 +0300

    working on sign-in component interface.Made custom input and button components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import LoginRegister from "./pages/login-and-register/login-and-register.component"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/Login" component={LoginRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/login-and-register/login-and-register.component.js
@@ -0,0 +1,14 @@
+import  React from 'react'
+
+import './login-and-register.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+
+const LoginRegister =() =>(
+    <div className="loginRegister">
+        <SignIn/>
+    </div>
+)
+
+export default LoginRegister;
