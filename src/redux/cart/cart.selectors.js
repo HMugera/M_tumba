997a3c827d4ff60142715c824b28e30a64bc7dec
Hmugera <mugerahughes@gmail.com>
2020-10-07 12:49:36 +0300

    updating our application with reselect,replaced cart selectors with memeoizedselectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -6,3 +6,10 @@ const selectCart = state => state.cart;
 export const selectCartItems = createSelector(
     [selectCart],cart => cart.cartItems
 )
+
+export const selectCartItemsCount =createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumaltedQuantity,cartItem) => accumaltedQuantity + cartItem.quantity,0
+    )
+)
